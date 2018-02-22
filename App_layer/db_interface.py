f68294917776f474ad78fb2245602ca05f936cb7
Tarekk <tarekkekhia@Tareks-MacBook-Pro.local>
2018-02-22 14:25:35 -0500

    rollback

--- App_layer/db_interface.py
+++ App_layer/db_interface.py
@@ -58,7 +58,7 @@ class OR_inputs(object):
 		left outer join
 		(select course_id,skill_id from Course_skills order by course_id,skill_id asc)y
 		on x.id = y.course_id and x.skill = y.skill_id
-		order by x.id,x.skill asc;''')
+		order by x.id,x.skill asc; ''')
 
 		data_skills = cur.fetchall()
 		all_skills = [x[1] for x in data_skills]
@@ -76,13 +76,13 @@ class OR_inputs(object):
 
 	def fetch_courseSkillLvls_matrix(self,num_courses):
 		cur.execute('''select x.id,
-		case when skill_lvl is null then 0 else 1 end as skill_lvl
-		from
-		(select x.id,s.id as 'skill' from Courses x cross join (select id from Skills order by id asc)s where x.course_provider_id = 3)x
-		left outer join
-		(select course_id,skill_id,skill_lvl from Course_skills order by course_id,skill_id asc)y
-		on x.id = y.course_id and x.skill = y.skill_id
-		order by x.id,x.skill asc;''')
+					case when skill_lvl is null then 0 else 1 end as skill_lvl
+					from
+					(select x.id,s.id as 'skill' from Courses x cross join (select id from Skills order by id asc)s where x.course_provider_id = 3 order by x.id,s.id asc)x
+					left outer join
+					(select course_id,skill_id,skill_lvl from Course_skills x inner join Courses y on x.course_id = y.id and y.course_provider_id = 3 order by course_id,skill_id asc)y
+					on x.id = y.course_id and x.skill = y.skill_id
+					order by x.id,x.skill asc;''')
 
 		data_skills = cur.fetchall()
 		all_skills = [x[1] for x in data_skills]
