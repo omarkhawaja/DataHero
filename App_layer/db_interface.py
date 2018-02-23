70be0c3a349ff9e44945cb4840400269b4f64ce7
Tarekk <tarekkekhia@Tareks-MacBook-Pro.local>
2018-02-23 16:45:30 -0500

    rly final

--- App_layer/db_interface.py
+++ App_layer/db_interface.py
@@ -58,7 +58,8 @@ class OR_inputs(object):
 		left outer join
 		(select course_id,skill_id from Course_skills order by course_id,skill_id asc)y
 		on x.id = y.course_id and x.skill = y.skill_id
-		order by x.id,x.skill asc; ''')
+		order by x.id,x.skill asc; 
+		''')
 
 		data_skills = cur.fetchall()
 		all_skills = [x[1] for x in data_skills]
@@ -75,13 +76,13 @@ class OR_inputs(object):
 
 	def fetch_courseSkillLvls_matrix(self,num_courses):
 		cur.execute('''select x.id,
-		case when skill_lvl is null then 0 else 1 end as skill_lvl
+		case when (y.skill_lvl is null or y.skill_lvl = 0) then 0 else 1 end as skill_lvl
 		from
 		(select x.id,s.id as 'skill' from Courses x cross join (select id from Skills order by id asc)s where x.course_provider_id = 3)x
 		left outer join
 		(select course_id,skill_id,skill_lvl from Course_skills order by course_id,skill_id asc)y
 		on x.id = y.course_id and x.skill = y.skill_id
-		order by x.id,x.skill asc;	
+		order by x.id,x.skill asc;		
 		''')
 
 		data_skills = cur.fetchall()
--- Backend/SQL/Untitled.sql
+++ Backend/SQL/Untitled.sql
@@ -1,11 +1,11 @@
 select x.id,
-case when skill_lvl is null then 0 else 1 end as skill_lvl
+case when (y.skill_lvl is null or y.skill_lvl = 0) then 0 else 1 end as skill_lvl
 from
 (select x.id,s.id as 'skill' from Courses x cross join (select id from Skills order by id asc)s where x.course_provider_id = 3)x
 left outer join
 (select course_id,skill_id,skill_lvl from Course_skills order by course_id,skill_id asc)y
 on x.id = y.course_id and x.skill = y.skill_id
-order by x.id,x.skill asc;
+order by x.id,x.skill asc;	
 
 
 
@@ -16,4 +16,4 @@ from
 left outer join
 (select course_id,skill_id from Course_skills order by course_id,skill_id asc)y
 on x.id = y.course_id and x.skill = y.skill_id
-order by x.id,x.skill asc;
+order by x.id,x.skill asc limit 100000;
