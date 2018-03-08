89054c8d7f7287ac62ddfb401c2e000b43d51a8e
Tarekk <tarekkekhia@Tareks-MacBook-Pro.local>
2018-03-07 22:54:43 -0500

    fix

--- App_layer/PythonModel.py
+++ App_layer/PythonModel.py
@@ -23,7 +23,7 @@ def run_algorithm(courses,courseSkills,courseLevel,cost,ratings,lengths,timeAllo
         m.update()
         # Set Objective Function
         #m.setObjective(quicksum((x[i] * normalized_cost_and_length_score[i])  for i in range(numCourses)), GRB.MINIMIZE)
-        m.setObjective(quicksum((x[i] * cost[i])  for i in range(numCourses)), GRB.MINIMIZE)
+        m.setObjective(quicksum((x[i])  for i in range(numCourses)), GRB.MINIMIZE)
         # Set Partitioning Constraints / Modified to only work with "neededSkills"
         for s in range(numSkills):
             m.addConstr(quicksum(x[i] * courseSkills[i][s] for i in range(numCourses)) >= neededSkills[s])
