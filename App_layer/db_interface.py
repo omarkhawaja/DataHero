9c1f101b7193be40e8222f5acd8da4beec24b34c
Tarekk <tarek.kekhia@gmail.com>
2018-03-11 22:50:15 -0400

    fix

--- App_layer/db_interface.py
+++ App_layer/db_interface.py
@@ -159,8 +159,7 @@ class OR_inputs(object):
 					'''.format(skills_query,self.provider))
 
 		data_course_scores = cur.fetchall()
-		print(skills_query)
-		print(data_course_scores)
+		print(data_course_scores[0])
 		course_scores = [x[0] for x in data_course_scores]
 
 	def fetch_tech_combinations(self,position):
